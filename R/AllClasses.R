f15cbe7684662a99f9f7759d7e4f7c7bd723570b
dcellwanger <dc.ellwanger.dev@gmail.com>
2018-07-27 20:35:53 -0700

    updated namespace

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,3 +1,8 @@
 #' @import methods
-#' @rawNamespace import(stats, except=weights)
+# #' @rawNamespace import(stats, except=weights)
+#' @importFrom stats aggregate anova as.dist coefficients lm mad
+#' @importFrom stats cophenetic cor dist ecdf fisher.test formula
+#' @importFrom stats integrate median na.fail p.adjust phyper predict
+#' @importFrom stats qnorm quantile residuals rgamma rnbinom sd
+#' @importFrom stats smooth.spline var wilcox.test
 NULL
